e3adb51917926db431cdefc9c764c377cdef8349
JasperBouwman <jphbouwman@gmail.com>
2025-12-10 21:00:32 +0100

    TPort 1.21.7
    See changelog.md

---
+++ adapters/V1_21_11/src/main/java/com/spaceman/tport/adapters/V1_21_11_Adapter.java
@@ -0,0 +1,26 @@
+package com.spaceman.tport.adapters;
+
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.server.network.PlayerConnection;
+import org.bukkit.craftbukkit.v1_21_R7.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class V1_21_11_Adapter extends V1_21_11_WaypointAdapter {
+    
+    @Override
+    public String getAdapterName() {
+        return "1.21.11";
+    }
+    
+    @Override
+    public Object getPlayerConnection(Player player) {
+        return ((CraftPlayer)player).getHandle().g;
+    }
+    
+    @Override
+    public void sendPlayerPacket(Player player, Object packet) {
+        PlayerConnection pc = (PlayerConnection) getPlayerConnection(player);
+        pc.b((Packet<?>) packet);
+    }
+    
+}
